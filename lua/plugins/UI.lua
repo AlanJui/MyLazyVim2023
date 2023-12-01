3f395700741b4b7824906e9c26026c5c29805d75
居正中 <AlanJui.1960@gmail.com>
2023-12-01 14:33:23 +0800

    加入 colorize 插件。

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
+  {
     "rcarriga/nvim-notify",
     opts = {
       timeout = 3000,
